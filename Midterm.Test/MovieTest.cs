76cfe8b9955e44ffef2b0cc4b38306ce6b9672d3
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-08-13 14:20:37 -0400

    Updated edit method and started updating tests.

--- Midterm.Test/MovieTest.cs
+++ Midterm.Test/MovieTest.cs
@@ -15,53 +15,53 @@ namespace Midterm.Test
 		}
 
 		[Theory]
-		[InlineData("Alien: Covenant")]
-		[InlineData("aliencovenant")]
-		[InlineData(@"Alien/\Covenant")]
-		[InlineData("Alien: Covenant53")]
-		[InlineData("")]
-		public void EditMovieTitleTest(string newTitle)
+		[InlineData("Alien: Covenant", true)]
+		[InlineData("aliencovenant", true)]
+		[InlineData(@"Alien/\Covenant", true)]
+		[InlineData("Alien: Covenant53", true)]
+		[InlineData("", false)]
+		public void EditMovieTitleTest(string newTitle, bool expected)
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
-			testMovie.EditMovie("title", newTitle);
 
-			Assert.Equal(newTitle, testMovie.Title);
+			Assert.Equal(expected, testMovie.EditMovie("title", newTitle));
 		}
 
 		[Theory]
-		[InlineData("Action", MovieGenre.Action, true)]
-		[InlineData("action", MovieGenre.Action, true)]
-		[InlineData("ACTION", MovieGenre.Action, true)]
-		[InlineData("ACtiOn", MovieGenre.Action, true)]
-		[InlineData("Drama", MovieGenre.Drama, true)]
-		[InlineData("drama", MovieGenre.Drama, true)]
-		[InlineData("DRAMA", MovieGenre.Drama, true)]
-		[InlineData("dRAmA", MovieGenre.Drama, true)]
-		[InlineData("Horror", MovieGenre.Horror, true)]
-		[InlineData("horror", MovieGenre.Horror, true)]
-		[InlineData("HORROR", MovieGenre.Horror, true)]
-		[InlineData("HORror", MovieGenre.Horror, true)]
-		[InlineData("horor", "", false)]
-		public void EditMovieGenreTest(string newGenre, MovieGenre expectedgenre, bool expectedBool)
+		[InlineData("Action", true)]
+		[InlineData("action", true)]
+		[InlineData("ACTION", true)]
+		[InlineData("ACtiOn", true)]
+		[InlineData("Horror", true)]
+		[InlineData("horror", true)]
+		[InlineData("HORROR", true)]
+		[InlineData("HORror", true)]
+		[InlineData("horor", false)]
+		[InlineData("actin", false)]
+		[InlineData("", false)]
+		public void EditMovieGenreTest(string newGenre, bool expected)
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
-			testMovie.EditMovie("genre", newGenre);
-
-			Assert.Equal(expectedgenre, testMovie.Genre);
+			
+			Assert.Equal(expected, testMovie.EditMovie("genre", newGenre));
 		}
 
 
-		[Fact]
-		public void EditMovieDirectorTest1()
+		[Theory]
+		[InlineData("Ridley Scott", true)]
+		[InlineData("ridley scott", true)]
+		[InlineData(@"RIDLEY/\scott", true)]
+		[InlineData("ridlEY53", true)]
+		[InlineData("", false)]
+		public void EditMovieDirectorTest(string newDirector, bool expected)
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
-			testMovie.EditMovie("director", "Ridley Scott");
-
-			Assert.Equal("Ridley Scott", testMovie.Director);
+			
+			Assert.Equal(expected, testMovie.EditMovie("director", newDirector));
 		}
 
 		[Fact]
-		public void EditMovieRuntimeTest1()
+		public void EditMovieRuntimeTest()
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
 			testMovie.EditMovie("runtime", "122");
