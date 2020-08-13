54a6e3e778ee5b5f00507c38179cae76fca0e5ce
Eric Slutz <EricSlutz@QuickenLoans.com>
2020-08-13 19:45:42 -0400

    Updated edit feature.

--- Midterm.Test/MovieTest.cs
+++ Midterm.Test/MovieTest.cs
@@ -60,41 +60,68 @@ namespace Midterm.Test
 			Assert.Equal(expected, testMovie.EditMovie("director", newDirector));
 		}
 
-		[Fact]
-		public void EditMovieRuntimeTest()
+		[Theory]
+		[InlineData("122", true)]
+		[InlineData("10", true)]
+		[InlineData("100000", true)]
+		[InlineData("1", true)]
+		[InlineData("0", false)]
+		[InlineData("-100", false)]
+		[InlineData("twothousand", false)]
+		[InlineData("hello", false)]
+		public void EditMovieRuntimeTest(string newRuntime, bool expected)
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
-			testMovie.EditMovie("runtime", "122");
-
-			Assert.Equal(122, testMovie.Runtime);
+			
+			Assert.Equal(expected, testMovie.EditMovie("runtime", newRuntime));
 		}
 
-		[Fact]
-		public void EditMovieYearTest1()
+		[Theory]
+		[InlineData("2017", true)]
+		[InlineData("1980", true)]
+		[InlineData("1965", true)]
+		[InlineData("2020", true)]
+		[InlineData("0", false)]
+		[InlineData("1862", false)]
+		[InlineData("twothousand", false)]
+		[InlineData("hello", false)]
+		public void EditMovieYearTest(string newYear, bool expected)
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
-			testMovie.EditMovie("year", "2017");
 
-			Assert.Equal(2017, testMovie.ReleaseYear);
+			Assert.Equal(expected, testMovie.EditMovie("year", newYear));
 		}
 
-		[Fact]
-		public void EditMovieCastTest1()
+		[Theory]
+		[InlineData("Michael Fassbender, Katherine Waterston, Billy Crudup", true)]
+		[InlineData("Fassbender, Waterston, Crudup", true)]
+		[InlineData("Michael, Katherine Waterston, Billy Ray Crudup", false)]
+		[InlineData("Michael Fassbender", true)]
+		[InlineData("Michael Fassbender, Katherine Waterston", true)]
+		[InlineData("MICHAEL fassbender, katherine WATERSTON, billy crudup", true)]
+		[InlineData("Michael Fassbender Katherine Waterston", false)]
+		[InlineData("Michael Fassbender, @Katherine Waterston", false)]
+		[InlineData("Michael Fassbender Katherine Waterston3", false)]
+		[InlineData("Michael-Fassbender Katherine Waterston", false)]
+		[InlineData("", false)]
+		public void EditMovieCastTest(string newCast, bool expected)
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
-			testMovie.EditMovie("cast", "Michael Fassbender, Katherine Waterston, Billy Crudup");
-			List<string> result = new List<string> { "Michael Fassbender", "Katherine Waterston", "Billy Crudup" };
 
-			Assert.Equal(result, testMovie.Cast);
+			Assert.Equal(expected, testMovie.EditMovie("cast", newCast));
 		}
 
-		[Fact]
-		public void EditMovieDescriptionTest1()
+		[Theory]
+		[InlineData("The crew of a colony ship discover an uncharted planet with a threat beyond their imagination.", true)]
+		[InlineData("I can say whatever I want here.", true)]
+		[InlineData(@"HI/ \there", true)]
+		[InlineData("1234567890", true)]
+		[InlineData("", false)]
+		public void EditMovieDescriptionTest(string newDescription, bool expected)
 		{
 			Movie testMovie = new Movie("Aliens", MovieGenre.SciFi, "James Cameron", 1986, 137, new List<string> { "Sigourney Weaver", "Michael Biehn", "Bill Paxton", "Carrie Henn" }, "Aliens are going to get you in space.");
-			testMovie.EditMovie("description", "The crew of a colony ship discover an uncharted planet with a threat beyond their imagination.");
-
-			Assert.Equal("The crew of a colony ship discover an uncharted planet with a threat beyond their imagination.", testMovie.Description);
+			
+			Assert.Equal(expected, testMovie.EditMovie("description", newDescription));
 		}
 	}
 }
